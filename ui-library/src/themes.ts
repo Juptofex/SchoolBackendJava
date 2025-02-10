e26c8e536e9a9902338536f18dc5d3687b54b246
julien-damilot-vinci <julien.damilot@gmail.com>
2025-02-10 11:21:06 +0100

    added tutorial from CAE

---
+++ ui-library/src/themes.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#bd30ec",
+    },
+    secondary: {
+      main: "#5fec30",
+    },
+  },
+});
+
+export default theme;
