4e825843944ef9d5f25f627a09800c50451b26c5
juptofex <julien.damilot@gmail.com>
2025-02-11 15:22:56 +0100

    feat: exo - Nearly-finished

---
+++ BackendJava/fiche2/src/main/java/be/vinci/cae/fiche2/models/Drink.java
@@ -0,0 +1,60 @@
+package be.vinci.cae.fiche2.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "drinks")
+public class Drink {
+    public Drink() {}
+
+    public Drink(String name, String description, float price, Boolean alcoholic) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.alcoholic = alcoholic;
+    }
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(unique = true)
+    private String name;
+    private String description;
+    private float price;
+    private Boolean alcoholic;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public Boolean getAlcoholic() {
+        return alcoholic;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setAlcoholic(Boolean alcoholic) {
+        this.alcoholic = alcoholic;
+    }
+
+
+}
